587d2a27b29c2bbad32cc015d30a282ff6aa975d
catbaron- <catbaron@live.cn>
2016-10-24 22:15:37 +0900

    Play for fun

---
+++ 1024forFun/42.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define Mostly (
+#define Universe "\n\n101010\n\n"
+#define to printf
+#define Harmless )
+#define Panic main
+#define Dont int
+#define Answer {
+#define the /*42*/
+#define of /*42*/
+#define Question /*42*/
+#define Life /*42*/
+#define Ultimate (
+#define And )
+#define Everything ;}
+
+Dont Panic
+Mostly Harmless
+Answer to the Ultimate Question of Life the Universe And Everything
